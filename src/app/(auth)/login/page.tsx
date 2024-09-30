4fdd7fdd2de781525a12bdcac11e024cbf2b8c47
akashh <akashyoungstar123@gmail.com>
2024-09-30 11:08:24 +0530

    auth error

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import axios from "axios";
-import Lottie from "lottie-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { NextAuthProvider } from "./Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthProvider>
           <Toaster richColors />
           {children}
-        </NextAuthProvider>
       </body>
     </html>
   );
