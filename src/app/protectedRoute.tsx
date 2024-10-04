3d8822ef19d5d96135674593e23f1538bb2ba44c
akashh <akashyoungstar123@gmail.com>
2024-10-04 13:27:33 +0530

    jwt setItem

--- src/app/protectedRoute.tsx
+++ src/app/protectedRoute.tsx
@@ -3,24 +3,42 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { ComponentType } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const withProtectedRoute = <P extends object>(
   WrappedComponent: ComponentType<P>
 ) => {
   const HOC = (props: P) => {
     const router = useRouter();
-    const [isLoading, setIsLoading] = useState(true); 
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
-      const token = Cookies.get("access_token");  
+      const token = Cookies.get("access_token");
       const JwtToken = localStorage.getItem("jwtToken");
-
-      console.log('checking if the token exist or not', JwtToken);
-      
+      console.log("checking if the token exist or not", JwtToken);
       if (!JwtToken) {
-        localStorage.removeItem("jwtToken"); 
+        localStorage.setItem("ProEmail", "");
+        localStorage.removeItem("jwtToken");
         router.push("/login");
       } else {
-        setIsLoading(false);
+        try {
+          const decodedToken = jwtDecode(JwtToken); // Decode the JWT token
+          const currentTime = Date.now() / 1000; // Current time in seconds (JWT exp is in seconds)
+          console.log("decodedToken", decodedToken, currentTime);
+
+          if (decodedToken?.exp && decodedToken.exp < currentTime) {
+            localStorage.setItem("ProEmail", "");
+            localStorage.removeItem("jwtToken");
+            router.push("/login");
+          } else {
+            // Token is still valid
+            setIsLoading(false);
+          }
+        } catch (error) {
+          console.error("Invalid token:", error);
+          localStorage.setItem("ProEmail", "");
+          localStorage.removeItem("jwtToken");
+          router.push("/login");
+        }
       }
     }, [router]);
 
